fd4993fe6f165b02546cf368af775b0517af771c
vang007 <https://github.com/vang007>
2021-11-30 23:22:18 -0600

    server updated

--- all files/public/assets/js/animals.js
+++ all files/public/assets/js/animals.js
@@ -2,10 +2,10 @@ const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
-  console.log(resultArr);
+        console.log(resultArr);
 
-  const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
-    return `
+        const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
+                    return `
   <div class="col-12 col-md-5 mb-3">
     <div class="card p-3" data-id=${id}>
       <h4 class="text-primary">${name}</h4>
